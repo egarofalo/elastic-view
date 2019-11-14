25da516454d7c376ab39d1806e066fb2775912f8
Edgardo Garofalo <egarofalo83@gmail.com>
2019-11-14 09:50:50 -0300

    - Added _x WordPress function to Twig into the I10n trait.

--- Extension/I10n.php
+++ Extension/I10n.php
@@ -8,14 +8,24 @@ trait I10n
 {
     public function addI10n()
     {
-        $this->trans();
+        $this->translate();
+        $this->translateWithGetTextContext();
     }
 
-    public function trans()
+    public function translate()
     {
         if (function_exists('__')) {
             $this->twig->addFunction(new TwigFunction('__', function (string $text, string $domain = 'default') {
                 return __($text, $domain);
+            }, ['is_safe' => ['html']]));
+        }
+    }
+
+    public function translateWithGetTextContext()
+    {
+        if (function_exists('_x')) {
+            $this->twig->addFunction(new TwigFunction('_x', function (string $text, string $context, string $domain = 'default') {
+                return _x($text, $context, $domain);
             }, ['is_safe' => ['html']]));
         }
     }

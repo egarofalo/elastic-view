2c6b54760bfbc661fcfcc34d92f8aa11be4fcfb4
Edgardo Garofalo <egarofalo83@gmail.com>
2019-11-14 16:17:58 -0300

    - Added wp_get_attachment_image WordPress function to Twig into the PostThumbnailTemplate trait.

--- Extension/PostThumbnailTemplate.php
+++ Extension/PostThumbnailTemplate.php
@@ -9,6 +9,7 @@ trait PostThumbnailTemplate
     public function addPostThumbnailTemplate()
     {
         $this->getThePostThumbnail();
+        $this->wpGetAttachmentImage();
     }
 
     public function getThePostThumbnail()
@@ -16,6 +17,15 @@ trait PostThumbnailTemplate
         if (function_exists('get_the_post_thumbnail')) {
             $this->twig->addFunction(new TwigFunction('get_the_post_thumbnail', function ($post = null, $size = 'post-thumbnail', $attr = '') {
                 return get_the_post_thumbnail($post, $size, $attr);
+            }, ['is_safe' => ['html']]));
+        }
+    }
+
+    public function wpGetAttachmentImage()
+    {
+        if (function_exists('wp_get_attachment_image')) {
+            $this->twig->addFunction(new TwigFunction('wp_get_attachment_image', function (int $attachment_id, $size = 'thumbnail', bool $icon = false, $attr = '') {
+                return wp_get_attachment_image($attachment_id, $size, $icon, $attr);
             }, ['is_safe' => ['html']]));
         }
     }
